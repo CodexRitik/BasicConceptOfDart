90c098bed3a496a759f663af490aee7fb81fcc37
Ritik Bhushan Yadav <ritik.yadav_cs19@gla.ac.in>
2021-05-23 15:41:38 +0530

    Unary Operator

---
+++ Scratch3.dart
@@ -0,0 +1,14 @@
+//Unary Operators In dart
+void main(){
+  var x = 30;   
+  print(x++);   //post decrement
+     
+  var y = 25;  
+  print(++y);   //pre increment
+         
+  var z = 10;  
+  print(--z);   //pre decrement
+  
+  var u = 12;                                           
+  print(u--);    //post decrement
+}
