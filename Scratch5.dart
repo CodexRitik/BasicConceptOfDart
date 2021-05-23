a9a1f9bbb4d4ffc4b01dd2546edb3f35601e9b3a
Ritik Bhushan Yadav <ritik.yadav_cs19@gla.ac.in>
2021-05-23 15:46:21 +0530

    Relational Operators

---
+++ Scratch5.dart
@@ -0,0 +1,12 @@
+//Relational Operator in dart
+void main() {
+  var number = 30;
+  var number2 = 20;
+  print("$number > $number2 = ${number > number2}");//greater then
+  print("$number < $number2 = ${number < number2}");//less then
+  print("$number >= $number2 = ${number >= number2}");//greater or equal
+  print("$number <= $number2 = ${number <= number2}");//less or equal
+  print("$number == $number2 = ${number == number2}");//equql to
+  print("$number != $number2 = ${number != number2}");//not equal
+
+}
