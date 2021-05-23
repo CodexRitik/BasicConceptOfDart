2306ca5761d6275ebae485bf963948c883a712ff
Ritik Bhushan Yadav <ritik.yadav_cs19@gla.ac.in>
2021-05-23 15:42:44 +0530

    Assignment Operator

---
+++ Scratch4.dart
@@ -0,0 +1,15 @@
+//Assignment Operator in dart
+void main() {
+  var number = 30;
+  var number2 = 10;
+  number += number2; //number = number + number2
+  print(number);
+  number -= number2; //number = number - number2
+  print(number);
+  number ~/= number2; //number = number ~/ number2
+  print(number);
+  number *= number2; //number = number * number2
+  print(number);
+  number %= number2; //number = number % number2
+  print(number);
+}
